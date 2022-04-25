70877f1eab6863ae4fb29cf62d77e0de4ca33be0
Ege Sucu <egesucu@icloud.com>
2022-04-25 20:04:28 +0300

    Initial Commit

---
+++ SendMailApp/SendMailAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendMailAppApp.swift
+//  SendMailApp
+//
+//  Created by Ege Sucu on 25.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SendMailAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
